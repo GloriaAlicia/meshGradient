c8c3340b5035bbab8adbd375a2470fef00ea75dd
GloriaAlicia <ga365743@gmail.com>
2022-06-08 19:32:06 -0500

    primer degradado sin input

--- scripts/random-gradient-.js
+++ scripts/random-gradient-.js
@@ -1,15 +1,21 @@
-let button = document.querySelector("button")
+let button = document.querySelector("button");
 button.addEventListener("click",()=>{
-    changeColor(document.querySelector("p"));
-})
-let color2 = "#50bfc8";//correct
-let color1 = "#90fceac0"; // --button-save-modal
-let color3 = "#50bec86e"; //--light-correct:#ee83758e;
-let color4 = color3
+    let color1 = document.getElementById("color1").value;
+    let color2 = document.getElementById("color2").value;
+    let color3 = document.getElementById("color3").value;
+    let color4 = document.getElementById("color4").value;
 
-function changeColor(element){
-    element.style.backgroundImage = "radial-gradient(farthest-side at "+ randomNumber(0,100,"%") +" "+ randomNumber(0,100,"%") +","+ color1 +" 0%,rgba(0,0,0,0)70%), radial-gradient(farthest-side at "+ randomNumber(0,100,"%") +" "+ randomNumber(0,100,"%") +","+ color2 +" 0%,rgba(0,0,0,0)80%),    radial-gradient(circle farthest-side at "+ randomNumber(0,100,"%") +" "+ randomNumber(0,100,"%") +","+ color3 +" 0%,rgba(0,0,0,0)100%),radial-gradient(circle farthest-side at "+ randomNumber(0,100,"%") +" "+ randomNumber(0,100,"%") +","+ color4 +" 0%,rgba(0,0,0,0)100%)"
+    changeColor(document.querySelector(".gradient"), color1, color2, color3, color4);
+})
+
+function changeColor(element, color1, color2, color3, color4){
+    let generatedGradient = "radial-gradient(farthest-side at "+ randomNumber(0,100,"%") +" "+ randomNumber(0,100,"%") +","+ color1 +" 0%,rgba(0,0,0,0)70%), radial-gradient(farthest-side at "+ randomNumber(0,100,"%") +" "+ randomNumber(0,100,"%") +","+ color2 +" 0%,rgba(0,0,0,0)80%),    radial-gradient(circle farthest-side at "+ randomNumber(0,100,"%") +" "+ randomNumber(0,100,"%") +","+ color3 +" 0%,transparent),radial-gradient(circle farthest-side at "+ randomNumber(0,100,"%") +" "+ randomNumber(0,100,"%") +","+ color4 +" 0%,transparent)"
+
+
+    element.style.backgroundImage = generatedGradient;
+    document.getElementById("output").textContent = "background-image:" + generatedGradient + ";" + "background-color:" + document.getElementById("background").value + ";";
     element.style.backgroundRepeat = "no-repeat";
+    element.style.backgroundColor = document.getElementById("background").value;
 }
 //-----------------funciones utiles----------------
 function randomNumber(min,max,unit){
